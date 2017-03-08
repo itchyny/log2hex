bd587ffd6472046e91e70ea45b129363bb8de751
itchyny <itchyny@hatena.ne.jp>
2017-03-08 22:32:05 +0900

    add log2hex main program

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::env;
+
+extern crate log2hex;
+use log2hex::*;
+
+fn main() {
+    let place: u32 = env::args().nth(1).and_then(|x| x.parse().ok()).unwrap_or(0);
+    print!("{}: ", place);
+    for i in 0..8 {
+        print!("{}", log2hex(place + 4 * i as u32));
+        if (i + 1) % 8 == 0 {
+            print!("\n")
+        } else {
+            print!(" ")
+        }
+    }
+}
