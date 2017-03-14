67bb36ef2d9fc849583d8b88e0bbac15a90314a5
itchyny <itchyny@hatena.ne.jp>
2017-03-14 22:34:09 +0900

    rename powmod to pow_mod

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 pub fn log2hex(place: u32) -> String {
     let d: u32 = 4 * place;
     let fraction1: f64 = (1..d + 1)
-        .map(|i| (powmod(2, d - i, i as u64) as f64) / (i as f64))
+        .map(|i| (pow_mod(2, d - i, i as u64) as f64) / (i as f64))
         .sum();
     let fraction2: f64 = (d + 1..)
         .map(|i| 2.0_f64.powi(d as i32 - i as i32) / (i as f64))
@@ -19,13 +19,13 @@ pub fn log2hex(place: u32) -> String {
         .fold(String::new(), |s, t| s + &t)
 }
 
-fn powmod(n: u64, m: u32, d: u64) -> u64 {
+fn pow_mod(n: u64, m: u32, d: u64) -> u64 {
     if m == 0 {
         1 % d
     } else if m == 1 {
         n % d
     } else {
-        let k: u64 = powmod(n, m / 2, d);
+        let k: u64 = pow_mod(n, m / 2, d);
         if m % 2 == 0 {
             (k * k) % d
         } else {
